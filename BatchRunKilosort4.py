312add0e857a50f6bb8596757ea2dc9324f44a05
Katrina Deane <katdeane92@gmail.com>
2026-07-29 11:42:05 +0200

    generated unique name option for kilosort, added user selection of folder and detection of unprocessed bin files therein

--- BatchRunKilosort4.py
+++ BatchRunKilosort4.py
@@ -18,8 +18,9 @@ from kilosort import run_kilosort
 homedir=os.path.dirname("E:\Spike_kilosort_Riverside")
 
 filepath = "E:\\Spike_kilosort_Riverside\\Data\\240213_VMP06_kilo\\allego_4__uid0213-12-30-28_bulk.bin"
+resultpath = filepath + "\\newname"
 
-settings = {'filename': filepath, 'n_chan_bin': 32}
+settings = {'filename': filepath, 'n_chan_bin': 32, 'results_dir': resultpath}
 
 ops, st, clu, tF, Wall, similar_templates, is_ref, est_contam_rate, kept_spikes = \
     run_kilosort(
--- Data/240213_VMP06_kilo/kilosort4/params.py
+++
@@ -1,6 +0,0 @@
-n_channels_dat = 32
-offset = 0
-sample_rate = 30000
-dtype = 'int16'
-hp_filtered = False
-dat_path = ['E:/Spike_kilosort_Riverside/Data/240213_VMP06_kilo/allego_4__uid0213-12-30-28_bulk.bin']
